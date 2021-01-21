9298f60984384bf468d8f42098626443b98a4e55
marioguenzel <mario.guenzel@tu-dortmund.de>
2021-01-21 16:23:09 +0100

    cleanup communication

--- stt/communication.py
+++ stt/communication.py
@@ -1,57 +1,98 @@
-"""
-Creation of communication tasks.
-"""
+"""Creation of communication tasks."""
 
 import numpy as np
 import random
 import math
 import stt.task as task
 
+# CAN-bus description.
 CAN_BUS = {
-	'MESSAGE_BIT' : 130,
-	'BANDWIDTH_MBPS' : 1 #,
-	#'MAX_NODES' : 5
-	}
+    'MESSAGE_BIT': 130,
+    'BANDWIDTH_MBPS': 1
+}
 
 
-def non_preemptive_response_time(taskset): # help function
-	def time_demand_analysis_task(pivot, lower_prio_tasks=[], higher_prio_tasks=[], blocking=False):
-		blocked = max(task.wcet for task in lower_prio_tasks) if lower_prio_tasks else 0
-		probe = blocked + pivot.wcet + sum((task.wcet for task in higher_prio_tasks))
-		workload = 0
-		while probe <= pivot.deadline:
-			workload = blocked + task.wcet + sum((math.ceil(float(probe)/task.period)*task.wcet for task in higher_prio_tasks))
-			if not (workload > probe):
-				return workload
-			else:
-				probe = workload
-		return False
-	for i, task in enumerate(taskset):
-		rt = time_demand_analysis_task(task, taskset[i+1:], taskset[:i], True)
-		if rt > task.deadline:
-			return False
-		task.rt = rt
+# Main function
+def generate_communication_taskset(num_tasks, min_period, max_period,
+                                   rounded=False, max_trials=100):
+    """Generate a set of communication tasks.
+
+    num_tasks: number of communication tasks in the set
+    min_period: lower bound for the periods
+    max_period: upper bound for the periods
+    rounded: flag for rounding the period values
+    max_trials: maximal tries to create a proper task set
+    """
+    # If no proper taskset was created, we try again.
+    trials = 0
+    while (trials < max_trials):
+        # Create candidates.
+        taskset = generate_communication_candidate_taskset(
+                num_tasks, min_period, max_period, rounded)
+        taskset = sorted(taskset, key=lambda x: x.priority)
+
+        # Compute WCRT.
+        if non_preemptive_response_time(taskset):
+            return taskset
+        trials += 1
+    # The creation failed too many times.
+    return False
 
 
-def generate_communication_candidate_taskset(num_tasks, min_period, max_period, rounded=False): # help function
-	taskset = []
-	wcet = (float(CAN_BUS['MESSAGE_BIT'])/CAN_BUS['BANDWIDTH_MBPS'])/10**3
-	periods = np.exp(
-		np.random.uniform(low=np.log(min_period), high=np.log(max_period), size=num_tasks))
-	periods = np.rint(periods).tolist() if rounded else periods
-	prio = list(range(num_tasks))
-	random.shuffle(prio)
-	for i in range(num_tasks):
-		taskset.append(task.Task(i, 0, wcet, wcet, periods[i], periods[i], prio[i], True))
-	return taskset
+# Help functions
+def generate_communication_candidate_taskset(num_tasks, min_period, max_period,
+                                             rounded=False):
+    """Generate candidate for the set of communication tasks."""
+    taskset = []
+    # Generate WCET and periods.
+    wcet = (float(CAN_BUS['MESSAGE_BIT'])/CAN_BUS['BANDWIDTH_MBPS'])/10**3
+    periods = np.exp(np.random.uniform(
+            low=np.log(min_period), high=np.log(max_period), size=num_tasks))
+    if rounded:  # round to nearest integer.
+        periods = np.rint(periods).tolist()
+    # Generate priorities.
+    prio = list(range(num_tasks))
+    random.shuffle(prio)
+    # Create tasks.
+    for i in range(num_tasks):
+        taskset.append(task.Task(i, 0, wcet, wcet, periods[i], periods[i],
+                                 prio[i], True))
+    return taskset
 
 
-def generate_communication_taskset(num_tasks, min_period, max_period, rounded=False, max_trials=100): # main function
-	taskset = generate_communication_candidate_taskset(num_tasks, min_period, max_period, rounded)
-	taskset = sorted(taskset, key=lambda x: x.priority)
-	trials = 0
-	while True and (trials < max_trials):
-		if not (non_preemptive_response_time(taskset) is False):
-			return taskset
-		trials+=1
-	return False
+def non_preemptive_response_time(taskset):
+    """Compute the worst-case response time of the communication tasks."""
+    # Help function.
+    def tda_np(pivot, lower_prio_tasks=[], higher_prio_tasks=[]):
+        """Non-preemptive time demand analysis."""
+        # Blocking by lower priority tasks due to non-preemptiveness.
+        if lower_prio_tasks:
+            blocked = max(task.wcet for task in lower_prio_tasks)
+        else:
+            blocked = 0
+
+        # Increase time according to TDA:
+        time = (blocked + pivot.wcet
+                + sum(task.wcet for task in higher_prio_tasks))
+        workload = 0
+        while time <= pivot.deadline:
+            workload = (blocked + task.wcet + sum(
+                    math.ceil(float(time)/task.period) * task.wcet
+                    for task in higher_prio_tasks))
+            if (workload <= time):  # stop property
+                return workload
+            else:
+                time = workload
+        return False  # TDA failed
+
+    # Compute WCRT of each task.
+    for i, task in enumerate(taskset):
+        rt = tda_np(task, taskset[i+1:], taskset[:i], True)
+        if rt is False:  # tda_np failed
+            return False
+        elif rt > task.deadline:  # WCRT > deadline is not allowed
+            return False
+        else:
+            # Set task WCRT
+            task.rt = rt
+    return True
